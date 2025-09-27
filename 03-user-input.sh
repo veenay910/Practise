c986b6112376922e9679ac710b340559f4ee39e2
veenay910 <veenay910@gmail.com>
2025-09-27 10:18:10 +0530

    02

--- 03-user-input.sh
+++ 03-user-input.sh
@@ -2,5 +2,5 @@
 
 #The read command with option -p allows you prompt message along with takin user input
 
-read -p "Please enter the date" date
+read -p "Please enter the date: " date
 echo "$date"
