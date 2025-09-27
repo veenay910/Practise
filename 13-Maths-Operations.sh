db6e14503da5a206d5bcc7271add59f00622917c
veenay910 <veenay910@gmail.com>
2025-09-27 15:57:53 +0530

    13

---
+++ 13-Maths-Operations.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#performing arthematica operation without storing
+# To perform the multiple mathematical operation without storing them in a separate variable
+# write the operation inside the echo
+
+read -p "first value is: " X
+read -p "second value is " Y
+
+echo "Addition: (($X+$Y))"  
