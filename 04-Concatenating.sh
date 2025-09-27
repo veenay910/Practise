435726b7b07f8ac2d97c65b305393648cf39923e
veenay910 <veenay910@gmail.com>
2025-09-27 10:30:03 +0530

    02

--- 04-Concatenating.sh
+++ 04-Concatenating.sh
@@ -10,3 +10,14 @@ country=India
 info="${state} ${country}"
 
 echo "$info"
+
+echo "-------------------------------------------"
+
+school=$1
+class=$2
+
+echo " Enter the school name:" $1
+echo "Enter the class name:" $2
+
+echo "school = $school"
+echo "class = $class"
