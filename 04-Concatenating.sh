7cd47eabdc4cca0c5487338a2f5aa3ca3c06e13a
veenay910 <veenay910@gmail.com>
2025-09-27 10:24:07 +0530

    02

---
+++ 04-Concatenating.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+Concatenating multiple vriables
+# To concatenate multiple varibles and store them into a single variable, enclose them with a "" and 
+# write the variable with in {} consecutively
+
+state=AP
+country=India
+
+info "{$state} {$country}"
+
+echo "$info"
